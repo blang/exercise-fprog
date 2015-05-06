7551f1562304655c5ca111c258fa516ebe74ff83
Benedikt Lang <github@benediktlang.de>
2015-05-06 11:24:24 +0200

    task2 format

--- exercise03/task2.hs
+++ exercise03/task2.hs
@@ -5,15 +5,15 @@ fac :: Integral a => a -> a
 fac n = if n==0 then 1 else n * fac (n-1)
 
 
--- Fakultaetszaehlen
-facs :: Integer -> [Integer]
 -- facs 0 = [0!, 1!]
 -- facs 1 = [0!, 1!, 2!]
+-- Fakultaetszaehlen
+facs :: Integer -> [Integer]
 facs n = [k | i<-[0..n], let k = fac i]
 
 
-genList :: (Integer->a) -> Integer -> [a]
 -- genList f n = [f 0, f 1, ..., f (n-1)]
+genList :: (Integer->a) -> Integer -> [a]
 genList f n  = [k | i<-[0..(n-1)], let k = f i]
 
 testGenList = genList id 10
@@ -24,8 +24,3 @@ isPrime x = null [y | y<-[2..floor (sqrt (fromIntegral x))], x `mod` y == 0]
 
 factors :: Integer->[Integer]
 factors x = [y | y<-[2..(x-1)], x `mod` y == 0]
-
-
---https://stackoverflow.com/questions/4541415/haskell-prime-test
-isPrime' :: Integer -> Bool
-isPrime' x = null (filter (\y ->  x `mod`y == 0) (takeWhile (\y ->  y*y <= x) [2..]))
