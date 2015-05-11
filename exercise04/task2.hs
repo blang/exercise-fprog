3ddd61d35092b3194c3adcc05f70462fd5b371fd
Benedikt Lang <github@benediktlang.de>
2015-05-11 16:47:04 +0200

    task2 fertig

--- exercise04/task2.hs
+++ exercise04/task2.hs
@@ -13,20 +13,29 @@ mapBTree :: (a -> b) -> BTree a -> BTree b
 mapBTree f (Leaf x) = (Leaf (f x))
 mapBTree f (Node x y z) = (Node (mapBTree f x) (f y) (mapBTree f z))
 
-depthFirstPre :: BTree a -> [a]
-depthFirstPre (Leaf x) = [x]
-depthFirstPre (Node x y z) = y : (depthFirstPre x) ++ (depthFirstPre z)
+-- Teilaufgabe b
+foldBTree :: (a->b) -> (b -> a -> b -> b) -> BTree a -> b
+foldBTree f g (Leaf x) = f x
+foldBTree f g (Node l m r) = g (foldBTree f g l) m (foldBTree f g r)
 
-depthFirstIn :: BTree a -> [a]
-depthFirstIn (Leaf x) = [x]
-depthFirstIn (Node x y z) = (depthFirstIn x) ++ [y] ++ (depthFirstIn z)
+-- Teilaufgabe c
+preOrderBTreeLeaf :: a->[a]
+preOrderBTreeLeaf x = [x]
 
-depthFirstPost :: BTree a -> [a]
-depthFirstPost (Leaf x) = [x]
-depthFirstPost (Node x y z) = (depthFirstPost x) ++ (depthFirstPost z) ++ [y]
+preOrderBTreeNode :: [b] -> b -> [b] -> [b]
+preOrderBTreeNode l m r = [m] ++ l ++ r
 
-breadthFirst :: BTree a -> [a]
-breadthFirst (Leaf x) = [x]
-breadthFirst (Node x y z) = (breadthFirst x) ++ [y] ++ (breadthFirst z)
+testPreOrder = foldBTree preOrderBTreeLeaf preOrderBTreeNode testBaum == preOrder testBaum
 
+-- Hilfsfunktionen
+preOrder :: BTree a -> [a]
+preOrder (Leaf x) = [x]
+preOrder (Node x y z) = y : (preOrder x) ++ (preOrder z)
 
+inOrder :: BTree a -> [a]
+inOrder (Leaf x) = [x]
+inOrder (Node x y z) = (inOrder x) ++ [y] ++ (inOrder z)
+
+postOrder :: BTree a -> [a]
+postOrder (Leaf x) = [x]
+postOrder (Node x y z) = (postOrder x) ++ (postOrder z) ++ [y]
