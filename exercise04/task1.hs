a4a88c5956e7791d023ebac4bf043e902f90a598
Benedikt Lang <github@benediktlang.de>
2015-05-11 15:30:39 +0200

    task 1 c, complete

--- exercise04/task1.hs
+++ exercise04/task1.hs
@@ -4,17 +4,17 @@ import Data.Char
 
 sumlen :: [Integer] -> Double
 sumlen l = avg where
-				red :: (Integer, Integer) -> Integer -> (Integer, Integer)
-				red (a,b) x = (a+x, b+1)
-				(sum, len) = foldl red (0,0) l
-				avg = (fromIntegral sum) / (fromIntegral len)
+                red :: (Integer, Integer) -> Integer -> (Integer, Integer)
+                red (a,b) x = (a+x, b+1)
+                (sum, len) = foldl red (0,0) l
+                avg = (fromIntegral sum) / (fromIntegral len)
 
 
 sumlen' :: [Integer] -> Double
 sumlen' l = avg where
-				sum = foldl (+) 0 l
-				len = toInteger $ length l
-				avg = (fromIntegral sum) / (fromIntegral len)
+                sum = foldl (+) 0 l
+                len = toInteger $ length l
+                avg = (fromIntegral sum) / (fromIntegral len)
 
 testSumlen :: [Integer] -> Bool
 testSumlen x = (sumlen x) == (sumlen' x)
@@ -22,10 +22,18 @@ testSumlen x = (sumlen x) == (sumlen' x)
 
 makeUpper :: String -> (Int, String)
 makeUpper l = (c, str) where
-				red :: (Int, [Char]) -> Char -> (Int, [Char])
-				red (x, y) n
-							| isLower n = (x+1, y ++ [(toUpper n)])
-							| otherwise = (x, y ++ [n])
-				(c, str) = foldl red (0, []) l
+                red :: (Int, [Char]) -> Char -> (Int, [Char])
+                red (x, y) n
+                            | isLower n = (x+1, y ++ [(toUpper n)])
+                            | otherwise = (x, y ++ [n])
+                (c, str) = foldl red (0, []) l
 
 testMakeUpper = makeUpper "LoremIpsum" == (8, "LOREMIPSUM")
+
+
+evalPoly :: Num a => [a] -> a -> a
+evalPoly l x =  let
+                    red :: Num a => a -> a -> a -> a
+                    red x a b = a+(b*x)
+                in foldr1 (red x) l
+testEvalPoly = evalPoly [3,2,0,5] 10 == 5023
